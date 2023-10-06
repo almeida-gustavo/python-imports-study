a95ef2c821f940a08dfa138a7ca2333aac96a32f
melogustavo <gustavomelo95@gmail.com>
2023-10-06 17:50:02 -0300

    update readme

--- setup_doc-entry_points/timmins/app_sub_folder/timmins.py
+++ setup_doc-entry_points/timmins/app_sub_folder/timmins.py
@@ -4,7 +4,9 @@ def main():
     display_eps = importlib.metadata.entry_points(group='timmins.display')
     try:
         print(display_eps)
-        display = display_eps[0].load()
+        display = display_eps["excl"].load()
+        # This lined is the value on /setup_doc-entry_points/timmins_plugin_fancy/setup.py
+        # display = display_eps["lined"].load()
         display('Hello world')
     except IndexError:
        default_output('Hello world - Im default')
